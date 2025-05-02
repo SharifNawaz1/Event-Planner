e5acfd4ddd90b9596abaffc1bec1edbcfec096ca
Sharif Nawaz <sharifkne355@gmail.com>
2025-05-02 23:07:32 +0530

    fix the cors issue

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config({path: "./config/config.env"})
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:["https://event-planner-1-7zor.onrender.com/"],
     methods:["POST"],
     credentials: true
 }))
